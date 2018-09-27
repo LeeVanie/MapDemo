94220497b4507b7750929c861093209684af3f28
LeeVanie <942629585@qq.com>
2018-09-27 15:02:14 +0800

    处理了重新运行崩溃异常

--- app/src/main/java/com/lee/map01/MainActivity.java
+++ app/src/main/java/com/lee/map01/MainActivity.java
@@ -1,11 +1,13 @@
 package com.lee.map01;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -56,6 +58,9 @@ import com.baidu.mapapi.search.poi.PoiSearch;
 import com.lee.map01.adapter.SreachAdapter;
 import com.lee.map01.overlay.MyOverLay;
 import com.lee.map01.overlayutil.PoiOverlay;
+import com.zhy.m.permission.MPermissions;
+import com.zhy.m.permission.PermissionDenied;
+import com.zhy.m.permission.PermissionGrant;
 
 import java.util.List;
 
@@ -205,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
-
+ 
         }
     };
 
@@ -214,6 +219,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        MPermissions.requestPermissions(this,
+                Common.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+
+        MPermissions.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @PermissionGrant(Common.ACCESS_COARSE_LOCATION)
+    public void requestSdcardSuccess() {
 
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
@@ -229,9 +254,15 @@ public class MainActivity extends AppCompatActivity {
         mPoiSearch = PoiSearch.newInstance();   //初始化检索功能
         mSreach = GeoCoder.newInstance();
         mSreach.setOnGetGeoCodeResultListener(listener);
-        
+
         setLinster();
     }
+
+    @PermissionDenied(Common.ACCESS_COARSE_LOCATION)
+    public void requestSdcardFailed() {
+        finish();
+    }
+    
     
     public void setLinster() {
         mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
